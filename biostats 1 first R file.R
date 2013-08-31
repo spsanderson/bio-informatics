dc0e6e9eadbbfcca7d1f8ed5aad4e737d4349476
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-08-31 19:15:25 -0400

    add comments to lines

--- biostats 1 first R file.R
+++ biostats 1 first R file.R
@@ -1,7 +1,20 @@
 # First R file for Biostatistics 1
+
+# This line produces a vector assigned to x of concatenated
+# values denoted by c()
 x = c(58,103,105,169,173,181,205,207,227,230,245,301,313)
+
+# This gets the minimum of x
 min(x)
+
+# This gets the maximum of x
 max(x)
+
+# This gets the meanof x
 mean(x)
+
+# This gets the median of x
 median(x)
+
+# This gets the standard deviation of x
 sd(x)
