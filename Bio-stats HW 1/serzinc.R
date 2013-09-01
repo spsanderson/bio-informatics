2215a70cf097fac44078ce626aae6b371bab5cdb
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-08-31 21:42:47 -0400

    update to script to format as.numeric

--- Bio-stats HW 1/serzinc.R
+++ Bio-stats HW 1/serzinc.R
@@ -1,3 +1,4 @@
 # This script assumes you have the necessary data file loaded into the current directory
 # and that you have it loaded into your workspace, this file was created using RSTudio
+serzinc <- as.numeric(do.call("rbind", sapply("serzinc.csv", read.csv)))
 summary(serzinc)
