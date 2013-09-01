02d94a4387ea6f3aa66b4e15bde68857fcb418bd
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-09-01 13:06:02 -0400

    ch3 homework script

---
+++ Bio-stats HW 1/hw1_ch3.R
@@ -0,0 +1,15 @@
+# HW 1 Chapter 3, this script assumes that data files are in your working directory
+# Import file as serum_zinc in order to work with file and not change anything with
+# chapter 2 homework serzinc files
+serum_zinc <- read.csv("~/bio-informatics/Bio-stats HW 1/serzinc.csv")
+
+# Summary of the serum_zinc file
+summary(serum_zinc)
+# (a) find mean, median, mode, sd, range and IRQ
+mean(serum_zinc$zinc)
+median(serum_zinc$zinc)
+
+# There is not a simple way to get the mode of a data set in R so we have to
+# use a little work around in order to get it
+mode <- table(as.vector(serum_zinc))
+names(mode)[mode == max(mode)]
