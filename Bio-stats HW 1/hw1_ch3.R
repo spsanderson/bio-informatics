e7c8888dba144a94b63ab5823380ea25354952bc
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-09-01 13:28:14 -0400

    updates to file

--- Bio-stats HW 1/hw1_ch3.R
+++ Bio-stats HW 1/hw1_ch3.R
@@ -14,5 +14,23 @@ median(serum_zinc$zinc)
 mode <- table(as.vector(serum_zinc))
 names(mode)[mode == max(mode)]
 sd(serum_zinc$zinc)
-range(serum_zinc$zinc)
-IQR(serum_zinc$zinc)
+rng = range(serum_zinc$zinc)
+IQR = IQR(serum_zinc$zinc)
+IQR
+rng
+# Chebychev description of data
+# 2 sd's
+ci2 <- range((mean(serum_zinc$zinc)+2*sd(serum_zinc$zinc)),
+      ((mean(serum_zinc$zinc)-2*sd(serum_zinc$zinc))))
+ci2
+
+# 3 sd's
+ci3 <- range((mean(serum_zinc$zinc)+3*sd(serum_zinc$zinc)),
+             ((mean(serum_zinc$zinc)-3*sd(serum_zinc$zinc))))
+ci3
+
+# IQRm ci2 and ci3 together
+IQR
+rng
+ci2
+ci3
