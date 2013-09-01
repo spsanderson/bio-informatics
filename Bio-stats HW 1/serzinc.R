56ec2c854317f749b68176686a276a57158c4d7e
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-08-31 22:28:30 -0400

    addition of problem 17

--- Bio-stats HW 1/serzinc.R
+++ Bio-stats HW 1/serzinc.R
@@ -1,5 +1,6 @@
 # This script assumes you have the necessary data file loaded into the current directory
-# and that you have it loaded into your workspace, this file was created using RSTudio
+# and that you have it loaded into your workspace, this file was created using RStudio
+# Problem 1
 serzinc <- as.numeric(do.call("rbind", sapply("serzinc.csv", read.csv)))
 summary(serzinc)
 plot(serzinc, main="Point Plot of serzinc data")
@@ -11,5 +12,21 @@ plot(serzinc_interval, xlab='Intervals', ylab="Count in Interval",
      main="Serzinc Data in 10 Intervals")
 
 # Get a table of the relative frequencies
-rel_freq <- table(serzinc_interval)
-rel_freq
+sz_rel_freq <- table(serzinc_interval)
+sz_rel_freq
+
+# 17 lowbwt
+# sex 1=male, tox 1=toxemia, grmhem 1=hemorrhage
+lowbwt <- as.numeric(do.call("rbind", sapply("lowbwt.csv", read.csv)))
+summary(lowbwt)
+plot(lowbwt, main="Point Plot of lowbwt data")
+hist(lowbwt, main="Histogram of lowbwt data")
+
+# Cut lowbwt data into intervals, 10 wide per interval then plot histogram
+lowbwt_interval <- cut(lowbwt, 10)
+plot(lowbwt_interval, xlab="Intervals", ylab='Count in Interval',
+     main="Lowbwt Data in 10 Intervals")
+
+# Get a table of the relative frequencies
+lowbwt_rl_freq <- table(lowbwt_interval)
+lowbwt_rl_freq
