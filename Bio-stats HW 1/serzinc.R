d98340b6913357b885571ac31c551de241ddd0cb
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-08-31 20:26:03 -0400

    update script

--- Bio-stats HW 1/serzinc.R
+++ Bio-stats HW 1/serzinc.R
@@ -1,1 +1,3 @@
+# This script assumes you have the necessary data file loaded into the current directory
+# and that you have it loaded into your workspace, this file was created using RSTudio
 summary(serzinc)
