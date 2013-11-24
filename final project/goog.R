7ca01235eddb7a0da6e851579b5b736dd7c7f93e
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-11-23 19:58:52 -0500

    minor update

--- final project/goog.R
+++ final project/goog.R
@@ -1,20 +1,51 @@
 require(quantmod)
+require(lattice)
 getSymbols("GOOG")
-goog <- GOOG[from = '2013-01-01', to = '2013-11-22']
-head(goog)
+getSymbols("MSFT")
 
-mean(dlyrt)
-sd(dlyrt)
-var(dlyrt)
-summary(dlyrt)
+goog <- GOOG[from = '2013-01-01', to = '2013-11-22']
+msft <- MSFT[from = '2013-01-01', to = '2013-11-22']
+
+head(goog)
+head(msft)
+
+summary(goog)
+summary(msft)
+
+meangadj <- mean(goog$GOOG.Adjusted)
+sdgadj <- sd(goog$GOOG.Adjusted)
+vargadj <- var(goog$GOOG.Adjusted)
+meangadj
+sdgadj
+vargadj
+
+meanmadj <- mean(msft$MSFT.Adjusted)
+sdmadj <- sd(msft$MSFT.Adjusted)
+varmadj <- var(msft$MSFT.Adjusted)
+meanmadj
+sdmadj
+varmadj
 
 plot(goog$GOOG.Adjusted)
+plot(msft$MSFT.Adjusted)
 chartSeries(goog)
+chartSeries(msft)
 
-dlyrt <- dailyReturn(goog)
-plot(dlyrt)
-logrt <- log(dlyrt)
-plot(logrt)
+dlyrtgoog <- dailyReturn(goog)
+dlyrtmsft <- dailyReturn(msft)
 
-hist(dlyrt)
-hist(logrt)
+plot(dlyrtgoog)
+plot(dlyrtmsft)
+
+logrtgoog <- log(dlyrtgoog)
+logrtmsft <- log(dlyrtmsft)
+
+plot(logrtgoog)
+plot(logrtmsft)
+
+hist(dlyrtgoog)
+hist(dlyrtmsft)
+
+hist(logrtgoog)
+hist(logrtmsft)
+
