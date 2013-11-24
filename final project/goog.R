6851988fdb466fd05f853e09ed2664e5bfcf9327
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-11-23 21:07:43 -0500

    minor update

--- final project/goog.R
+++ final project/goog.R
@@ -1,5 +1,6 @@
 require(quantmod)
-require(lattice)
+require(ggplot2)
+require(knitr)
 getSymbols("GOOG")
 getSymbols("MSFT")
 
@@ -32,7 +33,13 @@ chartSeries(goog)
 chartSeries(msft)
 
 dlyrtgoog <- dailyReturn(goog)
+mean(dlyrtgoog)
+sd(dlyrtgoog)
+var(dlyrtgoog)
 dlyrtmsft <- dailyReturn(msft)
+mean(dlyrtmsft)
+sd(dlyrtmsft)
+var(dlyrtmsft)
 
 plot(dlyrtgoog)
 plot(dlyrtmsft)
@@ -53,3 +60,38 @@ cor(goog, msft)
 
 t.test(goog$GOOG.Adjusted)
 t.test(msft$MSFT.Adjusted)
+
+p1 <- hist(dlyrtgoog)
+p2 <- hist(dlyrtmsft)
+plot( p1, col=rgb(0,0,1,1/4), xlim=c(-.2,.3))
+plot( p2, col=rgb(1,0,0,1/4), xlim=c(-.2,.3), add=T)
+
+## calculate the density - don't plot yet
+densgoog <- density(dlyrtgoog)
+densmsft <- density(dlyrtmsft)
+## calculate the range of the graph
+xlim <- range(densgoog$x,densmsft$x)
+ylim <- range(0, densgoog$y, densmsft$y)
+#pick the colours
+googCol <- rgb(1,0,0,0.2)
+msftCol <- rgb(0,0,1,0.2)
+## plot the carrots and set up most of the plot parameters
+plot(densgoog, xlim = xlim, ylim = ylim, xlab = 'Daily Returns',
+     main = 'Distribution of GOOG and MSFT Daily Returns', 
+     panel.first = grid())
+#put our density plots in
+polygon(densgoog, density = -1, col = googCol)
+polygon(densmsft, density = -1, col = msftCol)
+## add a legend in the corner
+legend('topleft',c('GOOG','MSFT'),
+       fill = c(googCol, msftCol), bty = 'n',
+       border = NA)
+
+logrtgoog
+logrtmsft
+
+p1 <- hist(logrtgoog)
+p2 <- hist(logrtmsft)
+plot( p1, col=rgb(0,0,1,1/4), xlim=c(-12,3), xlab = "Log Returns"
+      , main = "Histogram of Log Returns for Google and Microsoft")
+plot( p2, col=rgb(1,0,0,1/4), xlim=c(-12,3), add=T)
